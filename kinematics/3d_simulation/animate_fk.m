fa9df8b10586ba89fff1cedfb09c9373080da130
ardop <humanoid.ardop@gmail.com>
2016-07-05 19:43:03 +0530

    Equality errors and animation oscillations fixed

--- kinematics/3d_simulation/animate_fk.m
+++ kinematics/3d_simulation/animate_fk.m
@@ -1,8 +1,10 @@
 clc;
 
 initial_theta = [0, pi/2, pi/2, 0, 0];
+%sample angles
 % -1.5707 1.5707 1.5707 0 0
-
+% -0.756 0.984 0 -1.324 1.23
+theta_a = initial_theta;
 figure;
 fk_coord_plot(initial_theta);
 initialize_axes();
@@ -15,14 +17,18 @@ while(true)
     
     else
         theta_b = str2double(strsplit(in));
-        %Animating the rotation
+        %Animating the rotation. rounding off to avoid errors
         theta_b = round(theta_b, 2);
-        theta_a = round(initial_theta, 2);
+        theta_a = round(theta_a, 2);
         done = false;
         
         while ~done
             theta_check = theta_a - theta_b;
+            %Rounding off to avoid errors
+            theta_a = round(theta_a, 2);
+            theta_b = round(theta_b, 2);
             for i=1:5
+                %Updating angle at every time instance
                 if theta_check(i)<0
                     theta_a(i) = theta_a(i) + 0.01;
                 elseif theta_check(i)>0
@@ -32,14 +38,14 @@ while(true)
                 
             %Plotting
             fk_coord_plot(theta_a);
+            %Pause required for animation
             pause(0.01);
-
-            
-            %Checking equality
-            if sum(theta_a - theta_b)<0.001
+    
+            %Checking equality using threshold to avoid
+            %Matlab equality errors
+            if sum(abs(theta_a - theta_b))<0.001
                 done = true;
             end
-            disp(sum(theta_a-theta_b));
 
         end
         
