bda2e7f52b9757444c78fed025f50e6b721057fc
ardop <humanoid.ardop@gmail.com>
2017-03-06 14:46:34 +0530

    Motion

--- kinematics/3d_simulation/animate_ik_motion.m
+++ kinematics/3d_simulation/animate_ik_motion.m
@@ -27,47 +27,68 @@ while(true)
         target1 = str2double(in1);
         target2 = str2double(in2);
         
-        theta_b = ik_pseudo_inverse(target1');
-        theta_b = theta_b';
+        interval = 10;
+        xc = (target2(1) - target1(1))/interval;
+        yc = (target2(2) - target1(2))/interval;
+        zc = (target2(3) - target1(3))/interval;
         
-        if theta_b~=-1
         
-            done = false;
+        
+        theta_a = ik_pseudo_inverse(target1');
+        
+        done2 = true;
+        
+        while(done2)
+            
+            target1 = target1 + [xc yc zc];
+            
+            theta_b = ik_pseudo_inverse_initial(target1', theta_a(1:4)');
 
-            while ~done
-                theta_check = theta_a - theta_b;
-                %Rounding off to avoid errors
-                theta_a = round(theta_a, 2);
-                theta_b = round(theta_b, 2);
-                for i=1:5
-                    %Updating angle at every time instance
-                    if theta_check(i)<0
-                        theta_a(i) = theta_a(i) + 0.01;
-                    elseif theta_check(i)>0
-                        theta_a(i) = theta_a(i) - 0.01;
+            if theta_b~=-1
+
+                done = false;
+
+                while ~done
+                    theta_check = theta_a - theta_b;
+                    %Rounding off to avoid errors
+                    theta_a = round(theta_a, 2);
+                    theta_b = round(theta_b, 2);
+                    for i=1:5
+                        %Updating angle at every time instance
+                        if theta_check(i)<0
+                            theta_a(i) = theta_a(i) + 0.01;
+                        elseif theta_check(i)>0
+                            theta_a(i) = theta_a(i) - 0.01;
+                        end
                     end
+
+                    %Plotting
+                    fk_coord_plot(theta_a);
+                    %Pause required for animation
+                    pause(0.01);
+
+                    %Checking equality using threshold to avoid
+                    %Matlab equality errors
+                    if sum(abs(theta_a - theta_b))<0.001
+                        done = true;
+                    end
+
                 end
 
-                %Plotting
-                fk_coord_plot(theta_a);
-                %Pause required for animation
-                pause(0.01);
+            else
 
-                %Checking equality using threshold to avoid
-                %Matlab equality errors
-                if sum(abs(theta_a - theta_b))<0.001
-                    done = true;
-                end
+                fprintf('No Solution\n\n');
+                done2 = false;
 
             end
+
+            theta_a = theta_b;
             
-        else
-            
-            fprintf('No Solution\n\n');
+            if(target1(1)==target2(1) && target1(2)==target2(2) && target1(3)==target2(3))
+                break
+            end
             
         end
-        
-        theta_a = theta_b;
         
          
     end
