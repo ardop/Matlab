ee57311b1b2d111eece0266edd42d9eb7bd5000f
ardop <humanoid.ardop@gmail.com>
2017-03-08 17:10:04 +0530

    Motion simulator

---
+++ kinematics/3d_simulation/move_to_target.m
@@ -0,0 +1,43 @@
+function [theta_a] = move_to_target(target, theta_a)
+
+    theta_b = ik_pseudo_inverse(target');
+    theta_b = theta_b';
+
+    if theta_b~=-1
+
+        done = false;
+
+        while ~done
+            theta_check = theta_a - theta_b;
+            %Rounding off to avoid errors
+            theta_a = round(theta_a, 2);
+            theta_b = round(theta_b, 2);
+            for i=1:5
+                %Updating angle at every time instance
+                if theta_check(i)<0
+                    theta_a(i) = theta_a(i) + 0.01;
+                elseif theta_check(i)>0
+                    theta_a(i) = theta_a(i) - 0.01;
+                end
+            end
+
+            %Plotting
+            fk_coord_plot(theta_a);
+            %Pause required for animation
+            pause(0.01);
+
+            %Checking equality using threshold to avoid
+            %Matlab equality errors
+            if sum(abs(theta_a - theta_b))<0.001
+                done = true;
+            end
+
+        end
+
+    else
+
+        fprintf('No Solution\n\n');
+
+    end
+    
+    theta_a = theta_b;
